1424bfd0ce09e3a32485a02cbde9119b196151ed
Sammed-Chougule <sammedsanjaychougule@gmail.com>
2025-09-10 19:03:18 +0530

    added shadcn component

---
+++ src/app/login/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import { CardDemo } from '@/components/loginForm';
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div className='flex justify-center items-center h-screen '>
+           <CardDemo />
+        </div>
+    )
+}
+
+export default Page;
--- src/components/heroSectionOne.jsx
+++ src/components/heroSectionOne.jsx
@@ -113,10 +113,13 @@ const Navbar = () => {
                 <img src="/quizzers_logo.png" alt="Logo" className="h-8 w-8 md:h-12 md:w-12" />
                 <h1 className="text-base font-bold md:text-2xl">Quizzers</h1>
             </div>
+            <Link href="/login">
             <button
                 className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
                 Login
             </button>
+            </Link>
+            
         </nav>
     );
 };
